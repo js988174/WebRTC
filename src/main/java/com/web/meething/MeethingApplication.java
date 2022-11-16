f0095f06ba5e52e07568bbb64e212fb0e55fe103
js988174 <rudtjs4902@naver.com>
2022-11-16 15:13:16 +0900

    2022-11-16 - project + vue 적용

---
+++ src/main/java/com/web/meething/MeethingApplication.java
@@ -0,0 +1,13 @@
+package com.web.meething;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeethingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeethingApplication.class, args);
+	}
+
+}
